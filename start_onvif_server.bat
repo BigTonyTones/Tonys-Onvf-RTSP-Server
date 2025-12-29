e69724dbf9f757f20e2a4062ac44133c941e429e
BigTonyTones <shamelinc@gmail.com>
2025-12-29 14:41:32 -0800

    Update to v4.4: Linux System FFmpeg, removed emojis from logs

--- start_onvif_server.bat
+++ start_onvif_server.bat
@@ -6,7 +6,7 @@ REM ========================================
 
 echo.
 echo ========================================
-echo  Tonys Onvif-RTSP Server v4.3.1
+echo  Tonys Onvif-RTSP Server v4.4
 echo ========================================
 echo.
 
