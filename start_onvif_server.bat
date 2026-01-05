0924327ba038c63452eef13805ffc7182dd0705e
BigTonyTones <shamelinc@gmail.com>
2026-01-05 10:04:13 -0800

    Update version to 5.5.4

--- start_onvif_server.bat
+++ start_onvif_server.bat
@@ -6,7 +6,7 @@ REM ========================================
 
 echo.
 echo ========================================
-echo  Tonys Onvif-RTSP Server v5.5.3
+echo  Tonys Onvif-RTSP Server v5.5.4
 echo ========================================
 echo.
 
